cfcfbdc54fd527018291e9dd9f8b01110c0eed75
ritvij14 <ritvij2001@gmail.com>
2020-08-16 00:24:33 +0530

    Added prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
-import "./components/Greet";
-import Greet from "./components/Greet";
+import React from 'react';
+import './App.css';
+import './components/Greet';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      <Greet />
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Greet = () => {
   return <h1>Hello world!</h1>;
