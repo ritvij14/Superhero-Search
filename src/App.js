a77a012c40bf98d62aed8097532bf84aa8be978a
ritvij14 <ritvij2001@gmail.com>
2020-08-30 00:06:10 +0530

    added enter key functionality and sesionStorage

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ import Axios from 'axios';
 
 const url = 'https://superheroapi.com/api/257638918909912/search/';
 function App() {
-  const [heroes, setHeroes] = useState({ list: [] });
-  const [query, setQuery] = useState('');
+  const [heroes, setHeroes] = useState(
+    sessionStorage.getItem('list')
+      ? { list: JSON.parse(sessionStorage.getItem('list')) }
+      : { list: [] }
+  );
+  const [query, setQuery] = useState(
+    sessionStorage.getItem('query') ? sessionStorage.getItem('query') : ''
+  );
   const onChangeHandler = (e) => {
     setQuery(e.target.value);
+    sessionStorage.setItem('query', e.target.value);
   };
-  const searchHandler = async (e) => {
-    e.preventDefault();
+  const searchHandler = async () => {
     await Axios.get(url + query).then((heroesData) => {
       if (heroesData.response === 'error') {
         console.log(heroesData.response);
         setHeroes({ list: [] });
       } else {
         setHeroes({ list: heroesData.data.results });
+        sessionStorage.setItem('list', JSON.stringify(heroesData.data.results));
       }
     });
+  };
+  // handling enter key pressed
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      searchHandler();
+    }
   };
   return (
     <div className="App">
@@ -27,8 +40,10 @@ function App() {
         <input
           type="text"
           placeholder="Enter hero name"
+          value={query}
           className="mt-2 border-2 rounded focus:outline-none px-2"
           onChange={onChangeHandler}
+          onKeyDown={handleKeyPress}
         />
         <button
           onClick={searchHandler}
