fff475b12c7f9b563baefeb8d73a17ff19678e43
ritvij14 <ritvij2001@gmail.com>
2020-08-29 23:49:00 +0530

    removing tailwind generated file

--- src/App.js
+++ src/App.js
@@ -5,51 +5,47 @@ import Axios from 'axios';
 
 const url = 'https://superheroapi.com/api/257638918909912/search/';
 function App() {
-  const [heroes, setHeroes] = useState({ list: [], errorMessage: '' });
+  const [heroes, setHeroes] = useState({ list: [] });
   const [query, setQuery] = useState('');
   const onChangeHandler = (e) => {
     setQuery(e.target.value);
   };
   const searchHandler = async (e) => {
     e.preventDefault();
-    await Axios.get(url + query)
-      .then((heroesData) => {
-        if (heroesData.response === 'error') {
-          console.log(heroesData.response);
-          setHeroes({ list: [], errorMessage: heroesData.response });
-        } else {
-          setHeroes({ list: heroesData.data.results });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    await Axios.get(url + query).then((heroesData) => {
+      if (heroesData.response === 'error') {
+        console.log(heroesData.response);
+        setHeroes({ list: [] });
+      } else {
+        setHeroes({ list: heroesData.data.results });
+      }
+    });
   };
   return (
     <div className="App">
-      <div className="flex py-10 justify-center">
+      <div className="lg:flex py-10 justify-center">
         <input
           type="text"
           placeholder="Enter hero name"
-          className="border-2 rounded focus:outline-none px-2"
+          className="mt-2 border-2 rounded focus:outline-none px-2"
           onChange={onChangeHandler}
         />
         <button
           onClick={searchHandler}
-          className="bg-blue-500 focus:outline-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="mt-2 bg-blue-500 focus:outline-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Search
         </button>
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-        {heroes.list.length !== 0 ? (
-          heroes.map((hero) => (
+        {typeof heroes.list !== undefined && Array.isArray(heroes.list) ? (
+          heroes.list.map((hero) => (
             <div key={hero.id}>
               <HeroCard heroData={hero} />
             </div>
           ))
         ) : (
-          <h1>{heroes.errorMessage}</h1>
+          <h1>Sorry no heroes found for your search.</h1>
         )}
       </div>
     </div>
--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const HeroCard = ({ heroData }) => {
   const [name] = useState(heroData.name);
-  // console.log(heroData);
+  console.log(heroData);
   return (
     <div className="rounded mx-4 my-5 shadow-xl border-4 border-aqua">
       <img
@@ -10,7 +10,26 @@ const HeroCard = ({ heroData }) => {
         src={heroData.image.url}
         alt="hero_image"
       />
-      <h1>{name}</h1>
+      <h1 className="text-xl">{name}</h1>
+      <div>
+        <h2 className="text-lg">Powerstats:-</h2>
+        <div className="flex pl-2">
+          <p className="text-xs mx-1">
+            Intelligence: {heroData.powerstats.intelligence}
+          </p>
+          <p className="text-xs mx-1">
+            Strength: {heroData.powerstats.strength}
+          </p>
+          <p className="text-xs mx-1">Speed: {heroData.powerstats.speed}</p>
+        </div>
+        <div className="flex pl-2">
+          <p className="text-xs mx-1">
+            Durability: {heroData.powerstats.durability}
+          </p>
+          <p className="text-xs mx-1">Power: {heroData.powerstats.power}</p>
+          <p className="text-xs mx-1">Combat: {heroData.powerstats.combat}</p>
+        </div>
+      </div>
     </div>
   );
 };
