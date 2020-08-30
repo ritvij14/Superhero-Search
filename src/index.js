8e823727282a4a8214eba437517fa9e336e6242d
ritvij14 <ritvij2001@gmail.com>
2020-08-30 14:02:53 +0530

    changing css file used

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './tailwind.output.css';
+import './tailwind.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
