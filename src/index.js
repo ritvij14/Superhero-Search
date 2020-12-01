644b61ce01eb8cd4760d2c6b52d629845770b4b8
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2020-12-01 10:15:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './tailwind.min.css';
 
 ReactDOM.render(
